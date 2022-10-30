df36c91f63d915ab268e0b4b45213883b55d2ca8
Jayke <jaykegamer12@gmail.com>
2022-10-30 04:23:42 -0500

    listings changes

--- Apartment/Pages/Listings.cshtml.cs
+++ Apartment/Pages/Listings.cshtml.cs
@@ -12,15 +12,20 @@ namespace Apartment.Pages
 
     public class ApartmentList2
     {
+        public string name;
         public string img;
         public int cost;
         public double userRating;
+        public string ratingURL;
+        
         //int d;
-        public ApartmentList2(string _img, int _cost, double _userRating)
+        public ApartmentList2(string _name, string _img, int _cost, double _userRating, string _ratingURL)
         {
+            this.name = _name;
             this.img = _img;
             this.cost = _cost;
             this.userRating = _userRating;
+            this.ratingURL = _ratingURL;
         }
     }
 }
