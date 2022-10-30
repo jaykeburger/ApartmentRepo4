c2f6941e34a4c156ca8a0848a12adc1e3a4f82f3
Lizon132 <lizon132@gmail.com>
2022-10-30 12:13:43 -0500

    front page and imges

---
+++ Apartment/Pages/Violations.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Apartment.Pages
+{
+    public class ViolationsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
